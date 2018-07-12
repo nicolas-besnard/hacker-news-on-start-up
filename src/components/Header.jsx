2dce403cbc772d87cf04df084d29681384d50bf6
Nicolas Besnard <besnard.nicolas@gmail.com>
2018-07-12 17:42:20 +1000

    image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 
+import image from '../images/white-border.png'
+
 class Header extends Component {
   render() {
     return (
       <header className="header">
         <nav className="inner">
-          {/*<img src={image} alt="" className="logo"/>*/}
-          {/*<img alt="Y Combinator" src="http://www.ycombinator.com/images/ycombinator-logo-fb889e2e.png" className="logo" />*/}
-          {/*<a href="/top" className="router-link-exact-active router-link-active">Top</a>*/}
-          {/*<a href="/new" className="">New</a>*/}
-          {/*<a href="/show" className="">Show</a>*/}
-          {/*<a href="/ask" className="">Ask</a>*/}
-          {/*<a href="/job" className="">Jobs</a>*/}
+          <img src={image} alt="" className="logo"/>
         </nav>
       </header>
     );
